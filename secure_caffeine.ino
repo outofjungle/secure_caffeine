ffaed02e04c590148c2575727e9cf9c05904852d
Venkat Venkataraju <ven@yubico.com>
2018-10-01 12:15:47 -0700

    inital commit

---
+++ secure_caffeine.ino
@@ -0,0 +1,25 @@
+#include <Mouse.h>
+
+#define DELAY 1
+
+int r;
+float theta;
+
+void setup() {
+  Mouse.begin();
+  randomSeed(analogRead(0));
+  r = random(10, 20);
+  theta = 0;
+}
+
+void loop() {
+  theta = move_mouse(theta);
+  delay(DELAY);
+}
+
+float move_mouse(float theta) {
+    theta = theta - 0.1;
+    int move_x = r * cos(theta);
+    int move_y = r * sin(theta);
+    Mouse.move(move_x, move_y, 0);
+}
